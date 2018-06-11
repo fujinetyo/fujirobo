e1432eb069935a143a06b73466468802df845507
fujinetyo <fujinetyo@gmail.com>
2018-06-11 18:44:04 +0900

    プロジェクト ファイルを追加します。

---
+++ FUJI-ROBO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FUJI_ROBO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
